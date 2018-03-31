f32b001ed737a901578d55ba48d18ee9a31b74fd
TomazRak <32834895+TomazRak@users.noreply.github.com>
2018-03-31 13:37:02 +0200

    Add full poject
    Exclude 2 sharedassets0.assets.resS
    Because over 100MB

---
+++ Earth orbit sun/Assets/Planet_Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet_Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.up, 5 * Time.deltaTime);
+        transform.Rotate(0, 0.5f, 0);
+    }
+}
